b62f28522014ff82978a85e120cb046c218bb6e6
jozef-allen <joeashef@gmail.com>
2023-12-17 21:38:29 +0100

    Add project files.

---
+++ Entities/DBContext.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace database_api.Entities
+{
+    public partial class DBContext : DbContext
+    {
+        public DBContext()
+        {
+        }
+
+        public DBContext(DbContextOptions<DBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<User> User { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.UseCollation("utf8mb4_0900_ai_ci")
+                .HasCharSet("utf8mb4");
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.ToTable("user");
+
+                entity.Property(e => e.Id).ValueGeneratedNever();
+
+                entity.Property(e => e.EnrollmentDate).HasColumnType("datetime");
+
+                entity.Property(e => e.FirstName).HasMaxLength(45);
+
+                entity.Property(e => e.LastName).HasMaxLength(45);
+
+                entity.Property(e => e.Password).HasMaxLength(45);
+
+                entity.Property(e => e.Username).HasMaxLength(45);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ Entities/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace database_api.Entities
+{
+    public partial class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string Username { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public DateTime EnrollmentDate { get; set; }
+    }
+}
